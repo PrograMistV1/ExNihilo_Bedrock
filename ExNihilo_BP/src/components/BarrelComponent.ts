af55558e3e232bd9300b0cc49dd0a6a73fb1ac0d
PrograMistV1 <programist.v.1.1@gmail.com>
2026-03-14 18:31:33 +0100

    add dynamic filling and composting functionality to BarrelComponent

--- ExNihilo_BP/src/components/BarrelComponent.ts
+++ ExNihilo_BP/src/components/BarrelComponent.ts
@@ -1,17 +1,23 @@
 import {
+    Block,
     BlockComponentBlockBreakEvent,
     BlockComponentOnPlaceEvent,
     BlockComponentPlayerInteractEvent,
     BlockCustomComponent,
-    EntityInventoryComponent
+    Entity,
+    EntityInventoryComponent,
+    Player
 } from "@minecraft/server";
+import {BlockStateSuperset} from "@minecraft/vanilla-data";
 import {COMPOSTABLE_ITEMS} from "../data/compostable_items";
 import {getTileEntity} from "../Utils";
 
 export class BarrelComponent implements BlockCustomComponent {
 
     onPlace(e: BlockComponentOnPlaceEvent): void {
-        e.block.dimension.spawnEntity("exnihilo:barrel_tile", e.block.location);
+        const entity = e.block.dimension.spawnEntity("exnihilo:barrel_tile", e.block.location);
+        entity.setDynamicProperty("filling", 0);
+        entity.setDynamicProperty("type", "empty");
     }
 
     onBreak(e: BlockComponentBlockBreakEvent): void {
@@ -20,19 +26,74 @@ export class BarrelComponent implements BlockCustomComponent {
         getTileEntity(block, "exnihilo:barrel_tile")?.remove();
     }
 
-    onPlayerInteract(e: BlockComponentPlayerInteractEvent): void {
-        const player = e.player;
-        const inventory = player.getComponent("minecraft:inventory") as EntityInventoryComponent;
-
-        const container = inventory.container;
-        if (!container) return;
-
-        const item = container.getItem(player.selectedSlotIndex);
-        if (!item) return;
-
-        const value = COMPOSTABLE_ITEMS[item.typeId];
-        if (value !== undefined) {
-            player.sendMessage(`Compost +${value}`);
-        }
+    onPlayerInteract(e: BlockComponentPlayerInteractEvent) {
+        checkCompostable(e.block, e.player);
     }
+}
+
+function checkCompostable(block: Block, player: Player): void {
+    const tile = getTileEntity(block, "exnihilo:barrel_tile");
+    const inventory = player.getComponent("minecraft:inventory") as EntityInventoryComponent;
+    const container = inventory?.container;
+    const item = container?.getItem(player.selectedSlotIndex);
+    const fillAmount = item && COMPOSTABLE_ITEMS[item.typeId];
+
+    if (!tile || !container || !item || !fillAmount) return;
+    const filling = tile.getDynamicProperty("filling") as number;
+    const type = tile.getDynamicProperty("type") as string;
+    if (type != "empty" && type != "compost" || filling == 100) return;
+
+    if (item.amount > 1) {
+        item.amount--;
+        container.setItem(player.selectedSlotIndex, item);
+    } else {
+        container.setItem(player.selectedSlotIndex, null);
+    }
+    changeFilling(block, fillAmount, "compost");
+}
+
+function changeFilling(block: Block, amount: number, type: string): void {
+    const tile = getTileEntity(block, "exnihilo:barrel_tile");
+    if (!tile) return;
+
+    const oldFilling = tile.getDynamicProperty("filling") as number;
+    const oldType = tile.getDynamicProperty("type") as string;
+
+    const newFilling = Math.min(Math.max(oldFilling + amount, 0), 100); // 0..100
+    tile.setDynamicProperty("filling", newFilling);
+    tile.setDynamicProperty("type", type);
+
+    if (newFilling !== oldFilling || type !== oldType) {
+        onFillingChanged(tile, block);
+    }
+    //move the entity up if the level has increased
+    const diff = calculateLevel(newFilling) - calculateLevel(oldFilling);
+    if (diff > 0 && type != "water" && type != "lava") {
+        const pos = block.location;
+        const entities = block.dimension.getEntities({
+            excludeTypes: ["exnihilo:barrel_tile"],
+            location: {
+                x: pos.x + 0.5,
+                y: pos.y + 0.5,
+                z: pos.z + 0.5
+            },
+            maxDistance: 0.45
+        });
+        entities.forEach(entity => {
+            entity.applyImpulse({x: 0, y: 0.22 * diff, z: 0})
+        });
+    }
+}
+
+function onFillingChanged(tile: Entity, block: Block) {
+    const filling = tile.getDynamicProperty("filling") as number;
+    const type = tile.getDynamicProperty("type") as string;
+    const level = calculateLevel(filling);
+
+    block.setPermutation(block.permutation.withState("exnihilo:level" as keyof BlockStateSuperset, level));
+    block.setPermutation(block.permutation.withState("exnihilo:filling" as keyof BlockStateSuperset, type));
+}
+
+function calculateLevel(filling: number): number {
+    return filling > 0 ? 1 + Math.floor(Math.ceil(filling) / 25) : 0;
 }
