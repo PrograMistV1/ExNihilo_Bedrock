aeb8dc942ffcdfc7b25c6ddad795e5fc830995da
PrograMistV1 <programist.v.1.1@gmail.com>
2026-03-16 20:42:59 +0100

    add BARREL_TILE_ID constant and implement entity cleanup for barrel tiles

--- ExNihilo_BP/src/Main.ts
+++ ExNihilo_BP/src/Main.ts
@@ -1,6 +1,17 @@
-import {system} from "@minecraft/server";
+import {system, world} from "@minecraft/server";
 import {BarrelComponent} from "./components/BarrelComponent";
+import {BARREL_TILE_ID} from "./data/TileList";
 
 system.beforeEvents.startup.subscribe((initEvent) => {
     initEvent.blockComponentRegistry.registerCustomComponent('exnihilo:barrel', new BarrelComponent());
+
+    // Check all tiles for blocks every 5 minutes
+    system.runTimeout(() => {
+        for (const dimension of ["minecraft:nether", "minecraft:overworld", "minecraft:the_end"]) {
+            world.getDimension(dimension).getEntities({type: BARREL_TILE_ID}).forEach(entity => {
+                const comp = entity.dimension.getBlock(entity.location).getComponent("exnihilo:barrel");
+                if (!comp) entity.remove();
+            });
+        }
+    }, 6000)
 });
---
+++ ExNihilo_BP/src/data/TileList.ts
@@ -0,0 +1,3 @@
+export const BARREL_TILE_ID = "exnihilo:barrel_tile";
+export const SIEVE_TILE_ID = "exnihilo:sieve_tile";
+export const CRUCIBLE_TILE_ID = "exnihilo:crucible_tile";
