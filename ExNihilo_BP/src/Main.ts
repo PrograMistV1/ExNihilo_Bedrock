f75085dc778c41afe8c484f1bd92517ffe083f03
PrograMistV1 <programist.v.1.1@gmail.com>
2026-03-18 19:39:07 +0100

    refactor tile cleanup logic to improve bugged barrel tile removal and logging

--- ExNihilo_BP/src/Main.ts
+++ ExNihilo_BP/src/Main.ts
@@ -5,13 +5,23 @@ import {BARREL_TILE_ID} from "./data/TileList";
 system.beforeEvents.startup.subscribe((initEvent) => {
     initEvent.blockComponentRegistry.registerCustomComponent('exnihilo:barrel', new BarrelComponent());
 
-    // Check all tiles for blocks every 5 minutes
     system.runTimeout(() => {
-        for (const dimension of ["minecraft:nether", "minecraft:overworld", "minecraft:the_end"]) {
-            world.getDimension(dimension).getEntities({type: BARREL_TILE_ID}).forEach(entity => {
-                const comp = entity.dimension.getBlock(entity.location).getComponent("exnihilo:barrel");
-                if (!comp) entity.remove();
-            });
-        }
+        clearBuggedTiles();
+    }, 100);
+    // Check all tiles for blocks every 5 minutes
+    system.runInterval(() => {
+        clearBuggedTiles();
     }, 6000)
 });
+
+function clearBuggedTiles() {
+    for (const dimension of ["minecraft:nether", "minecraft:overworld", "minecraft:the_end"]) {
+        world.getDimension(dimension).getEntities({type: BARREL_TILE_ID}).forEach(entity => {
+            const comp = entity.dimension.getBlock(entity.location).getComponent("exnihilo:barrel");
+            if (!comp) {
+                console.log(`Removing bugged barrel tile at ${Math.floor(entity.location.x)}, ${Math.floor(entity.location.y)}, ${Math.floor(entity.location.z)} in dimension ${dimension}`);
+                entity.remove();
+            }
+        });
+    }
+}
