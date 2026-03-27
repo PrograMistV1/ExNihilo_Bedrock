a15829eb567d61ec4f02679fcf270c6db209db09
PrograMistV1 <programist.v.1.1@gmail.com>
2026-03-27 15:23:59 +0100

    Enhance CrucibleComponent with melting mechanics and lava extraction; update constants and input handling

--- ExNihilo_BP/src/components/CrucibleComponent.ts
+++ ExNihilo_BP/src/components/CrucibleComponent.ts
@@ -2,27 +2,57 @@ import {
     Block,
     BlockComponentBlockBreakEvent,
     BlockComponentPlayerInteractEvent,
+    BlockComponentTickEvent,
     BlockCustomComponent,
     EntityVariantComponent,
+    ItemStack,
     Player
 } from "@minecraft/server";
 import {consumeSelectedItem, getSelectedItemContext, getTileEntity} from "../Utils";
 import {CRUCIBLE_TILE_ID} from "../data/TileList";
-import {CRUCIBLE_CONSTANTS, CrucibleInput, InputGravel, MeltableBlocks, VARIANT_STATE_MAP} from "../data/CrucibleData";
+import {
+    CRUCIBLE_CONSTANTS,
+    CrucibleInput,
+    HeatRate,
+    InputDefault,
+    InputGravel,
+    InputLava,
+    MeltableBlocks,
+    VARIANT_STATE_MAP
+} from "../data/CrucibleData";
 
 export class CrucibleComponent implements BlockCustomComponent {
     onPlayerInteract(e: BlockComponentPlayerInteractEvent): void {
         handleMeltable(e.block, e.player);
+        handleExtractLava(e.block, e.player);
     }
 
     onBreak(e: BlockComponentBlockBreakEvent): void {
         getTileEntity(e.block, CRUCIBLE_TILE_ID)?.remove();
     }
+
+    onTick(e: BlockComponentTickEvent): void {
+        handleMeltingTick(e.block);
+    }
+}
+
+function handleMeltingTick(block: Block): void {
+    const input = getInputBlock(block);
+    const filling = getFilling(block);
+    if (input !== InputGravel || filling !== 100) return;
+
+    const heatRate = HeatRate[block.dimension.getBlock({x: block.x, y: block.y - 1, z: block.z}).typeId] ?? 0;
+    if (heatRate === 0) return;
+
+    if (incrementTimer(block, heatRate) > CRUCIBLE_CONSTANTS.MELTING_TIME_TICKS) {
+        setInputBlock(block, InputLava);
+        resetTimer(block);
+    }
 }
 
 function handleMeltable(block: Block, player: Player): void {
     const selectedItem = getSelectedItemContext(player);
-    const canInsertMeltable = getInputBlock(block) === undefined || getInputBlock(block) === InputGravel;
+    const canInsertMeltable = getInputBlock(block) === InputDefault || getInputBlock(block) === InputGravel;
     const fillAmount = MeltableBlocks[selectedItem.item?.typeId] ?? 0;
     if (!selectedItem.item || !canInsertMeltable || fillAmount === 0 || getFilling(block) === 100) return;
 
@@ -31,23 +61,42 @@ function handleMeltable(block: Block, player: Player): void {
     consumeSelectedItem(selectedItem);
 }
 
-function getInputBlock(crucible: Block): CrucibleInput | undefined {
+function handleExtractLava(block: Block, player: Player): void {
+    const selectedItem = getSelectedItemContext(player);
+    if (!selectedItem.item
+        || selectedItem.item.typeId !== "minecraft:bucket"
+        || getInputBlock(block) !== InputLava
+        || getFilling(block) !== 100) return;
+
+    setInputBlock(block, InputDefault);
+    selectedItem.container.setItem(selectedItem.slot, new ItemStack("minecraft:lava_bucket", 1));
+    block.dimension.playSound("bucket.fill_lava", block);
+}
+
+function getInputBlock(crucible: Block): CrucibleInput {
     const tile = getTileEntity(crucible, CRUCIBLE_TILE_ID);
-    if (!tile) return undefined;
+    if (!tile) return InputDefault;
 
     return VARIANT_STATE_MAP[tile.getComponent(EntityVariantComponent.componentId).value];
 }
 
-function setInputBlock(crucible: Block, input: CrucibleInput): void {
-    if (getTileEntity(crucible, CRUCIBLE_TILE_ID) !== undefined) return;
-
-    const pos = crucible.location;
-    const tile = crucible.dimension.spawnEntity(CRUCIBLE_TILE_ID, {
-        x: pos.x + CRUCIBLE_CONSTANTS.inputEntityCenterOffset,
-        y: pos.y + CRUCIBLE_CONSTANTS.inputEntityHeightOffset,
-        z: pos.z + CRUCIBLE_CONSTANTS.inputEntityCenterOffset
-    }, {spawnEvent: input});
-    tile.setDynamicProperty("filling", 0);
+function setInputBlock(block: Block, input: CrucibleInput): void {
+    let tile = getTileEntity(block, CRUCIBLE_TILE_ID);
+    if (tile !== undefined) {
+        if (input === InputDefault) {
+            tile.remove();
+            return;
+        }
+        tile.triggerEvent(input);
+    } else {
+        tile = block.dimension.spawnEntity(CRUCIBLE_TILE_ID, {
+            x: block.x + CRUCIBLE_CONSTANTS.CENTER_OFFSET,
+            y: block.y + CRUCIBLE_CONSTANTS.HEIGHT_OFFSET,
+            z: block.z + CRUCIBLE_CONSTANTS.CENTER_OFFSET
+        }, {spawnEvent: input});
+        tile.setDynamicProperty("filling", 0);
+        tile.setDynamicProperty("timer", 0);
+    }
 }
 
 function getFilling(crucible: Block): number {
@@ -65,8 +114,25 @@ function setFilling(crucible: Block, filling: number): void {
     const pos = tile.location;
     tile.teleport({
         x: pos.x,
-        y: Math.floor(pos.y) + CRUCIBLE_CONSTANTS.inputEntityHeightOffset + (filling / 100) * 0.75,
+        y: Math.floor(pos.y) + CRUCIBLE_CONSTANTS.HEIGHT_OFFSET + (filling / 100) * 0.75,
         z: pos.z,
     });
     tile.setDynamicProperty("filling", filling);
+}
+
+function incrementTimer(block: Block, amount: number): number {
+    const tile = getTileEntity(block, CRUCIBLE_TILE_ID);
+    if (!tile) return;
+
+    const currTime = tile.getDynamicProperty("timer") as number;
+    const newTime = currTime + amount;
+    tile.setDynamicProperty("timer", newTime);
+    return newTime;
+}
+
+function resetTimer(block: Block): void {
+    const tile = getTileEntity(block, CRUCIBLE_TILE_ID);
+    if (!tile) return;
+
+    tile.setDynamicProperty("timer", 0);
 }
--- ExNihilo_BP/src/data/CrucibleData.ts
+++ ExNihilo_BP/src/data/CrucibleData.ts
@@ -5,8 +5,9 @@ export const VARIANT_STATE_MAP: Readonly<Record<number, CrucibleInput>> = {
 }
 
 export const CRUCIBLE_CONSTANTS = {
-    inputEntityCenterOffset: 0.5,
-    inputEntityHeightOffset: 0.1875,
+    CENTER_OFFSET: 0.5,
+    HEIGHT_OFFSET: 0.1875,
+    MELTING_TIME_TICKS: 20
 }
 
 export type CrucibleInput = "exnihilo:default" | "exnihilo:gravel" | "exnihilo:lava";
@@ -18,4 +19,17 @@ export const InputLava = "exnihilo:lava";
 export const MeltableBlocks: Readonly<Record<string, number>> = {
     "minecraft:gravel": 10,
     "minecraft:cobblestone": 10,
+}
+
+export const HeatRate: Readonly<Record<string, number>> = {
+    "minecraft:torch": 1,
+    "minecraft:redstone_torch": 1,
+    "minecraft:magma": 2,
+    "minecraft:glowstone": 2,
+    "minecraft:shroomlight": 2,
+    "minecraft:lava": 3,
+    "minecraft:fire": 4,
+    "minecraft:soul_fire": 4,
+    "minecraft:campfire": 4,
+    "minecraft:soul_campfire": 4,
 }
