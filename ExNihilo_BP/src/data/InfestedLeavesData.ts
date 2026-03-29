1f6544358c486fe147c06ab0c8b3af70aa84575b
PrograMistV1 <programist.v.1.1@gmail.com>
2026-03-29 21:20:06 +0200

    update infested leaves

--- ExNihilo_BP/src/data/InfestedLeavesData.ts
+++ ExNihilo_BP/src/data/InfestedLeavesData.ts
@@ -6,10 +6,9 @@ export const LEAVES_TO_INFESTED_MAP: Readonly<Record<string, string>> = {
     "minecraft:acacia_leaves": "exnihilo:infested_acacia_leaves",
     "minecraft:dark_oak_leaves": "exnihilo:infested_dark_oak_leaves",
     "minecraft:mangrove_leaves": "exnihilo:infested_mangrove_leaves",
-    "minecraft:cherry_leaves": "exnihilo:infested_cherry_leaves",
     "minecraft:pale_oak_leaves": "exnihilo:infested_pale_oak_leaves",
     "minecraft:azalea_leaves": "exnihilo:infested_azalea_leaves",
-    "minecraft:flowering_azalea_leaves": "exnihilo:infested_flowering_azalea_leaves"
+    "minecraft:flowering_azalea_leaves": "exnihilo:infested_azalea_leaves"
 };
 
 export const LEAVES: Readonly<string[]> = [
@@ -34,10 +33,8 @@ export const INFESTED_LEAVES: Readonly<string[]> = [
     "exnihilo:infested_acacia_leaves",
     "exnihilo:infested_dark_oak_leaves",
     "exnihilo:infested_mangrove_leaves",
-    "exnihilo:infested_cherry_leaves",
     "exnihilo:infested_pale_oak_leaves",
-    "exnihilo:infested_azalea_leaves",
-    "exnihilo:infested_flowering_azalea_leaves"
+    "exnihilo:infested_azalea_leaves"
 ];
 
 export const DROP_CHANCES = {
