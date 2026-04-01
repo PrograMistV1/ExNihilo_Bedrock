123950fd270f928900d408730be5d357ddceb569
PrograMistV1 <programist.v.1.1@gmail.com>
2026-04-01 22:58:18 +0200

    Update meltable blocks

--- ExNihilo_BP/src/data/CrucibleData.ts
+++ ExNihilo_BP/src/data/CrucibleData.ts
@@ -17,8 +17,23 @@ export const InputGravel = "exnihilo:gravel";
 export const InputLava = "exnihilo:lava";
 
 export const MeltableBlocks: Readonly<Record<string, number>> = {
-    "minecraft:gravel": 10,
-    "minecraft:cobblestone": 10,
+    "minecraft:stone": 25,
+    "minecraft:cobblestone": 25,
+    "minecraft:andesite": 25,
+    "minecraft:basalt": 25,
+    "minecraft:blackstone": 25,
+    "minecraft:calcite": 25,
+    "minecraft:deepslate": 25,
+    "minecraft:diorite": 25,
+    "minecraft:dripstone": 25,
+    "minecraft:end_stone": 25,
+    "minecraft:granite": 25,
+    "minecraft:tuff": 25,
+    "minecraft:gravel": 20,
+    "minecraft:sand": 10,
+    "minecraft:obsidian": 100,
+    "minecraft:netherrack": 100,
+    "exnihilo:dust": 5
 }
 
 export const HeatRate: Readonly<Record<string, number>> = {
