23e22587e518f0abb2d26bf174bffb22d4708f70
PrograMistV1 <programist.v.1.1@gmail.com>
2026-04-07 19:46:14 +0200

    Add crushed variants for various blocks and update terrain textures

--- ExNihilo_BP/src/data/HammerData.ts
+++ ExNihilo_BP/src/data/HammerData.ts
@@ -1,5 +1,16 @@
 export const HAMMERABLE_BLOCKS_MAP = new Map<string, string>([
     ["minecraft:cobblestone", "minecraft:gravel"],
     ["minecraft:gravel", "minecraft:sand"],
-    ["minecraft:sand", "exnihilo:dust"]
+    ["minecraft:sand", "exnihilo:dust"],
+    ["minecraft:andesite", "exnihilo:crushed_andesite"],
+    ["minecraft:basalt", "exnihilo:crushed_basalt"],
+    ["minecraft:blackstone", "exnihilo:crushed_blackstone"],
+    ["minecraft:calcite", "exnihilo:crushed_calcite"],
+    ["minecraft:deepslate", "exnihilo:crushed_deepslate"],
+    ["minecraft:diorite", "exnihilo:crushed_diorite"],
+    ["minecraft:dripstone_block", "exnihilo:crushed_dripstone"],
+    ["minecraft:end_stone", "exnihilo:crushed_end_stone"],
+    ["minecraft:granite", "exnihilo:crushed_granite"],
+    ["minecraft:netherrack", "exnihilo:crushed_netherrack"],
+    ["minecraft:tuff", "exnihilo:crushed_tuff"]
 ]);
